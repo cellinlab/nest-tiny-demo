5327a5b9c3e00623840bf18c78cc1f4d5e8a51ca
cell <cellinlab@gmail.com>
2023-03-25 14:45:00 +0800

    feat: added logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+
+import { LoggerMiddleware } from './common/logger.middleware';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,4 +12,13 @@ import { CatsModule } from './cats/cats.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({
+        path: 'cats',
+        method: RequestMethod.GET,
+      });
+  }
+}
