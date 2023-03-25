aed48f5fdcd9970ba0669f04b5102b6aa7db99d7
cell <cellinlab@gmail.com>
2023-03-25 10:25:27 +0800

    feat: added cats Service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req } from '@nestjs/common';
 
 import { CreateCatDto, ListCatsDto, UpdateCatDto } from './dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) { }
+
   @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    return 'This action adds a new cat';
+  create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(@Query() query: ListCatsDto): string {
-    return 'This action returns all cats (filtered by query)';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Get(':id')
